f2246f343d096b7d3accc839769f91e05dc2443f
zanon005 <matheus.zanon@edu.pucrs.br>
2021-12-04 05:00:52 -0300

    Progresso

--- src/Enemies/Policial1.gd
+++ src/Enemies/Policial1.gd
@@ -4,53 +4,112 @@ export var run_speed := 300
 export var health := 100
 onready var animation := $PolicialAnimation
 var velocity := Vector2()
-var isDead := false
 
+var playerIsSpoted : bool = false
+var lastSpotedPlayerPos := Vector2.ZERO
+
+var currentDestinationPos := Vector2.ZERO
+var reachedCurrentDestinationPos := false
+
+enum STATE {ROAMING, CHASING_PLAYER, GOINGTO_LAST_KOWN_PLAYER_POS, DEAD}
+var currentState = STATE.ROAMING
+var currentRoamingIndexPoint = 0;
+
+var roamingPoints = []
 var path : Array = [] # Array com a lista de pontos para se mover
-onready var levelNavigation := get_node("LevelNavigation")
-onready var player := get_node("Player")
+var levelNavigation : Navigation2D = null
+var player = null
+onready var lineOfSight := $LineOfSight
 
 func _ready():
 	yield(get_tree(), "idle_frame")
 	var tree = get_tree()
 	if(tree.has_group('LevelNavigation')):
-		# 	Se tem 'LevelNavigation' entao busca o primeiro nodo filho dele
-		# no caso o tilemap 'ContornoEChao'
 		levelNavigation = tree.get_nodes_in_group("LevelNavigation")[0]
 	if(tree.has_group('Player')):
 		player = tree.get_nodes_in_group("Player")[0]
+	roamingPoints = tree.get_nodes_in_group("RoamingPoint")
+	pick_random_roaming_point()
 	animation.play("walk")
 	
+	
 func _physics_process(delta):
-	if(!isDead):
-		if(player && levelNavigation):
-			generate_path()
-			navigate()
+	if(player && currentState != STATE.DEAD):
+		lineOfSight.look_at(player.global_position)
+		_state_machine()
+		check_if_player_is_in_visible_range()
+		look_at(currentDestinationPos)
+		generate_path_to(currentDestinationPos)
+		navigate()
 		move()
+	print("CurrentState: '", STATE.keys()[currentState], "', GoingTo: [",currentRoamingIndexPoint,"] At: ",currentDestinationPos)
+		
+func _state_machine():
+	match currentState:
+			STATE.ROAMING:
+				if(playerIsSpoted):
+					currentState = STATE.CHASING_PLAYER
+				elif(check_if_reached_current_destination()):
+					pick_random_roaming_point()
+			STATE.CHASING_PLAYER:
+				if(!playerIsSpoted):
+					currentState = STATE.GOINGTO_LAST_KOWN_PLAYER_POS
+			STATE.GOINGTO_LAST_KOWN_PLAYER_POS:
+				if(playerIsSpoted):
+					currentState = STATE.CHASING_PLAYER
+				elif(check_if_reached_current_destination()):
+					currentState = STATE.ROAMING
+			STATE.DEAD:
+				pass
+
+func check_if_reached_current_destination() -> bool:
+	var aproxEqualX = int(global_position[0]) == int(currentDestinationPos[0])
+	var aproxEqualY = int(global_position[1]) == int(currentDestinationPos[1])
+	reachedCurrentDestinationPos = aproxEqualX && aproxEqualY
+	return reachedCurrentDestinationPos
+
+func check_if_player_is_in_visible_range() -> bool:
+	var lineOfSightcolision = lineOfSight.get_collider()
+	if(lineOfSightcolision && lineOfSightcolision.is_in_group("Player")):
+		playerIsSpoted = true
+		currentDestinationPos = player.global_position
+		lastSpotedPlayerPos = player.global_position
+		reachedCurrentDestinationPos = false
+		return true
+	playerIsSpoted = false
+	return false
+
+#Pega ponto aleatorio no mapa para ir que seja diferente do anterior
+func pick_random_roaming_point():
+	var indexRoamingPoint = randi() % len(roamingPoints)
+	while(indexRoamingPoint == currentRoamingIndexPoint):
+		indexRoamingPoint = randi() % len(roamingPoints)
+	currentDestinationPos = roamingPoints[indexRoamingPoint].global_position
+	print('Rand: ',currentRoamingIndexPoint, indexRoamingPoint)
+	currentRoamingIndexPoint = indexRoamingPoint
+	reachedCurrentDestinationPos = false
 
 func navigate(): #Defines the next position the enemy must moove
 	if(path.size() > 1):
 		velocity = global_position.direction_to(path[1]) * run_speed
-		
 		#	If the current global position is equal to current path point
 		# then pop this point that is already visited
 		if(global_position == path[0]):
 			path.pop_front()
 	
-func generate_path():
+func generate_path_to(destination : Vector2):
 	if levelNavigation != null and player != null:
-		path = levelNavigation.get_simple_path(global_position, player.global_position, false)
+		path = levelNavigation.get_simple_path(global_position, destination, false)
 
 func die():
-	if(!isDead):
+	if(currentState != STATE.DEAD):
 		var rand = randi() % 2
 		match rand:
 			0:
 				animation.play("die1")
 			1:
 				animation.play("die2")
-		isDead = true
-	#queue_free()
+		currentState = STATE.DEAD
 
 func move():
 	move_and_slide(velocity, Vector2.UP)
@@ -58,7 +117,7 @@ func move():
 
 #Only colides with "player_weapon" layer
 func _on_BodyHitBox_area_entered(area):
-	if(!isDead):
+	if(currentState != STATE.DEAD):
 		health = health - 50
 		if(health <= 0):
 			die()
